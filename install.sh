40c5e34af1bfb901d793ef407e4f6e8602a3cd9d
Mattio <78447457+Mattio-cmd@users.noreply.github.com>
2021-06-23 17:36:41 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! bin/sh
+cp ~/qtile-demon-slayer/qtile/config.py ~/.config/qtile
+cp ~/qtile-demon-slayer/kitty/kitty.conf ~/.config/kitty
+cp ~/qtile-demon-slayer/zsh/.zshrc ~/
+cd ~/.config/qtile
+mkdir icon
+mv ~/qtile-demon-slayer/qtile/icon/icon.png ~/config/qtile/icon
+sudo chsh -l
+sudo chsh -s /usr/bin.zsh
+
